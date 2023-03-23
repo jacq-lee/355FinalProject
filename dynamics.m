fe739aa95ece10b240b6b531107b5eace8ca61e4
Marie De Mey <97183312+mariedemeyy@users.noreply.github.com>
2023-03-23 11:46:41 -0400

    Initial get_fes_activation implementation
    - Implement get_fes_activation() function that takes in the variables associated to equation (5) in the paper, and the third state equation
    - Output of the get_fes_activation function is a function handle
    
    Next Steps:
     - Look at using WebPlotDigitizer to extract "fatigued" and "non-fatigued" values for k1, k2 and k3 from Fig. 10. in the paper
    - Consider using an 2 element array for k1, k2 and k3 where element 1 = non-fatigued value and element 2 = fatigued value. Initialize these elements by using a variable i, to make it easier to change between fatigued and non-fatigued simulation

--- dynamics.m
+++ dynamics.m
@@ -6,11 +6,24 @@ function [x_dot] = dynamics(x)
 % Output
 % x_dot: derivative of state vector
 
-% state equations
 x_dot = x; % initialize x_dot
-x_dot(1) = x(2); % theta dot (angular velocity)
-x_dot(2) = (torque_sol - torque_ta - gravity_moment(x(1)))/ankle_inertia; % angular acceleration
-x_dot(3) = get_velocity(tibialis_activation, x(3), norm_ltat); % normalized tibialis velocity
+
+% state equations from A2 (MDM: placeholders for now)
+x_dot(1) = x(2); % theta dot (angular velocity) - MDM: same as paper
+x_dot(2) = (torque_sol - torque_ta - gravity_moment(x(1)))/ankle_inertia; % angular acceleration - MDM: will change
+x_dot(3) = get_velocity(tibialis_activation, x(3), norm_ltat); % normalized tibialis velocity - MDM: will change
 x_dot(4) =  % activation
+
+% MDM - work for x_dot(3)
+
+% INITIAL WORK = BADD!!!!!
+% c0 = 13.2; % value from literature (doi:10.1123/jab.17.2.113)
+% [Q1_pw, Q2_f] = get_electrical_stimulation(pw, f);
+% x_dot(3) = c0*(-x(3) + Q1_pw*Q2_f + k3);
+
+%       OR
+% CLEANER WORK = FUNCTION HANDLER!!!!
+% [Q1_pw, Q2_f] = get_electrical_stimulation(pw, f);
+% x_dot(3) = get_fes_activation(Q1_pw, Q2_f, k3, x(3));
 
 end
--- get_fes_activation.m
+++ get_fes_activation.m
@@ -1,11 +1,24 @@
-function [] = get_fes_activation()
+% MDM
+% TODO: 
+% 1. Call get_electrical_stimulation() somewhere before this function is called
+%    (maybe in the main file) so Q1_pw and Q2_f are initialized. 
+% 2. Figure out best way to run different "simulations" and pass it
+%    different k3 based on the simulation we run
+
+
+function [fes_activation_fun] = get_fes_activation(Q1_pw, Q2_f, k3, x3)
 
 % Inputs
 % Q1_pw: electrical stimulation intensity
 % Q2_f: electrical stimulation frequency 
+% k3: FES intensity threshold for different individuals (identified through
+%     EKF algorithm and variable input in simulation)
+% x3: activation of electrical stimulation
 
 % Output
-% function handle maybe??
+% fes_activation_fun: function handle for activation equation (x_dot(3) equation)
 
+c0 = 13.2; % value from literature (doi:10.1123/jab.17.2.113)
+fes_activation_fun = @(x3) c0*(-x3 + Q1_pw*Q2_f + k3);
 
 end
