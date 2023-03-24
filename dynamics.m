12f6cacb8ec87da7c1ba2cba93a33c25b0084d6c
Jacqueline L <o.jacqueline.lee@gmail.com>
2023-03-24 17:21:31 -0400

    Update dynamics

--- dynamics.m
+++ dynamics.m
@@ -6,15 +6,21 @@ function [x_dot] = dynamics(x)
 % Output
 % x_dot: derivative of state vector
 
-x_dot = x; % initialize x_dot
+x_dot = zeros(size(x)); % initialize x_dot
+m = 0;
+d = 0;
+g = 9.81; % double check
 
-x_dot(1) = x(2); % theta dot (angular velocity)
-x_dot(2) = (torque_sol - torque_ta - gravity_moment(x(1)))/ankle_inertia; 
-
-
-x_dot(3) = get_velocity(tibialis_activation, x(3), norm_ltat); 
+k1 = 0;
+k2 = 0;
+k3 = 0;
 
 [Q1_pw, Q2_f] = get_electrical_stimulation(pw, f);
+
+
+x_dot(1) = x(2); % theta dot (angular velocity)
+x_dot(2) = (m*g*d*sin(x(1) - k1*e^(k2*x1) + torque_t + torque_e + torque_v))/(m*d^2); % angular acceleration
+x_dot(3) = get_velocity(); % normalized tibialis velocity
 x_dot(4) = get_fes_activation(Q1_pw, Q2_f, k3, x(4));
 
 end
