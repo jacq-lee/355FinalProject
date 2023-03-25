a411b9b16e21c3c689a405e32b07244b1ac4d8b0
marleyoconnell <65435020+marleyoconnell@users.noreply.github.com>
2023-03-25 17:13:03 -0400

    Updated passive force functions
    There were problems with get_passive_force_parallel and get_passive_force_series

--- get_passive_force_parallel.m
+++ get_passive_force_parallel.m
@@ -10,15 +10,11 @@ l_pe = 0.9;
 % Output
 % f_pe: passive force when muscle fibre longer than resting length (due to
 % parallel element)
-   f_pe = zeros(1,length(x3));
 
-   for n = 1:1:length(x3)
-       if x3(n) < l_pe
-           f_pe(n) = 0;
-       else 
-           f_pe(n) = (f_peak_iso*atan((0.1*(x3(n)-l_pe)-0.22)^10));
-        
-       end
-   end
+if x3 < l_pe
+   f_pe = 0;
+else 
+   f_pe = (f_peak_iso*atan((0.1*(x3-l_pe)-0.22)^10))
+end
 
 end
