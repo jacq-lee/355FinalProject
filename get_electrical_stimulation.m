c26fc361c31f428188b49ef98da7584bc9bc96fc
marleyoconnell <65435020+marleyoconnell@users.noreply.github.com>
2023-03-25 20:15:08 -0400

    Unit checking

--- get_electrical_stimulation.m
+++ get_electrical_stimulation.m
@@ -1,7 +1,7 @@
 function [Q1_pw, Q2_f] = get_electrical_stimulation(pw, f)
 
 % Inputs
-% pw: pulse width (microseconds)
+% pw: pulse width (seconds)
 % f: electrical stimulation frequency (Hz)
 
 % Outputs
