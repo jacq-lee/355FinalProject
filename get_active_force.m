0dfaab50a4d3bea99872df671fefa02225d4b02c
marleyoconnell <65435020+marleyoconnell@users.noreply.github.com>
2023-03-25 20:09:49 -0400

    get_velocity and get_active_force changes

--- get_active_force.m
+++ get_active_force.m
@@ -1,27 +1,21 @@
 function [f_ce] = get_active_force(x3, v_m, x4)
 
 % Inputs
-% x_3: normalized muscle length
-% V_m_bar: normalized velocity of muscle
+% x3: normalized muscle length
+% v_m: normalized velocity of muscle
+% x4: activation of electrical stimulation
 
 % Output
-% f_bar_ce: active force generated
+% f_bar_ce: normalized active force generated
 
-
-F_bar = 30; % EB: Normalized static force that is actually not really normalized?
-
-v_max = 2.5; % (m/s)
-vm_norm = v_m / v_max;
-
-% MDM: find what q_sf means, in paper said to be "activation signal q (s,
-% f)" so maybe this is x(4) 
-q_sf = x4% EB: unknown value, Activiation signal 
+F_bar = 30; % normalized static force
+q_sf = x4;  % Activiation signal q(s,f) - assumed to be equal to x4
 
 % Relationship between force and length (eq 9) - Normalized muscle force length
 f_Fl = exp(-40*(x3 -0.95)^4 + (x3 -0.95)^2);
 
 % Relationship between muscle output force and speed (eq 10) - Normalized muscle force speed factor
-f_Fv = 0.54*atan(5.69*vm_norm + 0.51) + 0.745;
+f_Fv = 0.54*atan(5.69*v_m + 0.51) + 0.745;
 
 f_ce = F_bar*q_sf*f_Fl*f_Fv;
 
--- get_velocity.m
+++ get_velocity.m
@@ -3,19 +3,18 @@ function [v_m] = get_velocity(x1, x2)
 % Inputs
 % x1: theta, ankle joint angular position
 % x2: theta dot, ankle joint angular velocity
-% l_mt: the length of the tendon plus the muscle fibre
 
 % Output
-% v_m: tibialis anterior muscle contraction velocity (not normalized)
+% v_m: normalized tibialis anterior muscle contraction velocity
 
 alpha_P = deg2rad(9.6); % pennation angle for the TA (feathering angle) (degrees)
-l_mt = tibialis_length(x1)
-
+l_mt = tibialis_length(x1);
+v_max = 2.5;
 
 step_size = 0.001;
-
 syms symbolic_x1
 
-l_deriv = inline(diff(tibialis_length(symbolic_x1)))
-v_m = x2*l_deriv(x1)*(1/cos(alpha_P)); % ta contraction velocity
+l_deriv = inline(diff(tibialis_length(symbolic_x1)));
+v_m_notnorm = x2*l_deriv(x1)*(1/cos(alpha_P)); % ta contraction velocity
+v_m = v_m_notnorm/v_max;
 end
