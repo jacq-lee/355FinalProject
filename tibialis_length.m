c5473663f44b46a2cfab205e0808d3ca3f79d46a
Jacqueline L <o.jacqueline.lee@gmail.com>
2023-04-13 01:19:39 -0400

    More coord system fixes

--- tibialis_length.m
+++ tibialis_length.m
@@ -34,35 +34,35 @@ tibialis_anterior_length = sqrt(difference(1)^2 + difference(2)^2);
 % tibialis_anterior_length = p_foot_ieff_ta - p_global_i_ta + p_shank_oeff_ta - p_global_o_ta
 
 %%
-% 
-% theta = deg2rad(-200):0.05:deg2rad(200)
-% lengthta = zeros(size(theta));
-% derivative = zeros(size(theta));
-% 
-% deriv_l_mt = @get_deriv_tibialis_length;
-% 
-% for i = 1:1:max(size(theta))
-%     x1 = theta(i);
-%     rotation = [cos(x1) -sin(x1) 
-%     sin(x1) cos(x1)];
-% 
-%     % coordinates in global reference frame
-%     origin = rotation * [0.3 -0.03]';
-%     insertion = [0.06, -0.03]';
-%     
-%     difference = origin - insertion;
-%     lengthta(i) = sqrt(difference(1)^2 + difference(2)^2);
-%     derivative(i) = deriv_l_mt(x1);
-%     
-% end
-% 
-% theta = rad2deg(theta);
-% figure
-% plot(theta, lengthta), hold on;
-% plot(theta, derivative), hold off;
-% legend('Length', 'Rate of Change')
-% xlabel('x1 (degrees)')
-% ylabel('TA Length')
-% grid on;
+
+theta = deg2rad(-200):0.05:deg2rad(200)
+lengthta = zeros(size(theta));
+derivative = zeros(size(theta));
+
+deriv_l_mt = @get_deriv_tibialis_length;
+
+for i = 1:1:max(size(theta))
+    x1 = theta(i);
+    rotation = [cos(x1) -sin(x1) 
+    sin(x1) cos(x1)];
+
+    % coordinates in global reference frame
+    origin = rotation * [0.3 -0.03]';
+    insertion = [0.06, -0.03]';
+    
+    difference = origin - insertion;
+    lengthta(i) = sqrt(difference(1)^2 + difference(2)^2);
+    derivative(i) = deriv_l_mt(x1);
+    
+end
+
+theta = rad2deg(theta);
+figure
+plot(theta, lengthta), hold on;
+plot(theta, derivative), hold off;
+legend('Length', 'Rate of Change')
+xlabel('x1 (degrees)')
+ylabel('TA Length')
+grid on;
 
 end
